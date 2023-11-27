0cd242047bec48dca3a432c17b504ac514e1917d
Julio César López Juárez <julio.ljuarez@gmail.com>
2023-11-27 14:39:04 -0600

    added name parameter

--- second_file.py
+++ second_file.py
@@ -1,7 +1,7 @@
 def print_hello_world():
     print("Hello World")
 
-def print_my_name():
-    print("Hi!, may name is Julio!")
+def print_my_name(name):
+    print(f"Hi!, may name is {name}!")
 
 print_hello_world()
